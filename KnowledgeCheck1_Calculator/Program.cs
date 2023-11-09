b64b5a4d7a350e4d2acc5f5ebc2eb3ede0bc001f
bjc-codelouisville <bj+codelouisville@cassadyweb.com>
2023-11-09 12:44:18 -0500

    Refactored: added separate method to validate user input integers and marked up code with comments

--- KnowledgeCheck1_Calculator/Program.cs
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -19,73 +19,75 @@ namespace KnowledgeCheck1_Calculator
             switch (input)
             {
                 case "1":
-                    Console.WriteLine("Enter 2 integers to add");
-                    var addNumber1 = Console.ReadLine();
-                    var addNumber2 = Console.ReadLine();
+                    Console.WriteLine("Enter 2 integers to add"); //original line
+                    var (addNumber1, addNumber2) = ValidateIntegers(); //run our new method
+                    var (addNumOne, addNumTwo) = (addNumber1, addNumber2); // extra line so we don't have to modify original variables
 
-                    if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
-                    {
-                        Console.Write($"{addNumber1} + {addNumber2} = ");
-                        Console.Write(calculator.Add(addNumOne, addNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
+                    Console.Write($"{addNumber1} + {addNumber2} = ");  //original line
+                    Console.Write(calculator.Add(addNumOne, addNumTwo)); //original line
                     break;
+                    //first instance of original code left in for reference
+                    //Console.WriteLine("Enter 2 integers to add");
+                    //var addNumber1 = Console.ReadLine();
+                    //var addNumber2 = Console.ReadLine();
+
+                    //if (int.TryParse(addNumber1, out int addNumOne) && int.TryParse(addNumber2, out int addNumTwo))
+                    //{
+                    //    Console.Write($"{addNumber1} + {addNumber2} = ");
+                    //    Console.Write(calculator.Add(addNumOne, addNumTwo));
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("One or more of the numbers is not an int");
+                    //}
+                    //break;
 
                 case "2":
-                    Console.WriteLine("Enter 2 integers to subtract");
-                    var subtractNumber1 = Console.ReadLine();
-                    var subtractNumber2 = Console.ReadLine();
+                    Console.WriteLine("Enter 2 integers to subtract"); //original line
+                    var (subtractNumber1, subtractNumber2) = ValidateIntegers(); //run our new method
+                    var (subNumOne, subNumTwo) = (subtractNumber1, subtractNumber2); // extra line so we don't have to modify original variables
 
-                    if (int.TryParse(subtractNumber1, out int subNumOne) && int.TryParse(subtractNumber2, out int subNumTwo))
-                    {
-                        Console.Write($"{subtractNumber1} - {subtractNumber2} = ");
-                        Console.Write(calculator.Subtract(subNumOne, subNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
+                    Console.Write($"{subtractNumber1} - {subtractNumber2} = ");  //original line
+                    Console.Write(calculator.Subtract(subNumOne, subNumTwo)); //original line
                     break;
 
                 case "3":
                     // Add code here
-                    Console.WriteLine("Enter 2 integers to multiply");
-                    var multiplyNumber1 = Console.ReadLine();
-                    var multiplyNumber2 = Console.ReadLine();
+                    Console.WriteLine("Enter 2 integers to add"); //original line
+                    var (multiplyNumber1, multiplyNumber2) = ValidateIntegers(); //run our new method
+                    var (mulNumOne, mulNumTwo) = (multiplyNumber1, multiplyNumber2); // extra line so we don't have to modify original variables
 
-                    if (int.TryParse( multiplyNumber1, out int mulNumOne) && int.TryParse( multiplyNumber2, out int mulNumTwo))
-                    {
-                        Console.Write($"{ multiplyNumber1} * { multiplyNumber2} = ");
-                        Console.Write(calculator.Multiply(mulNumOne, mulNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }break;
+                    Console.Write($"{multiplyNumber1} * {multiplyNumber2} = ");  //original line
+                    Console.Write(calculator.Multiply(mulNumOne, mulNumTwo)); //original line
+                    break;
 
                 case "4":
-                    Console.WriteLine("Enter 2 integers to divide");
-                    var divideNumber1 = Console.ReadLine();
-                    var divideNumber2 = Console.ReadLine();
+                    Console.WriteLine("Enter 2 integers to divide"); //original line
+                    var (divideNumber1, divideNumber2) = ValidateIntegers(); //run our new method
+                    var (divNumOne, divNumTwo) = (divideNumber1, divideNumber2); // extra line so we don't have to modify original variables
 
-                    if (double.TryParse(divideNumber1, out double divNumOne) && double.TryParse(divideNumber2, out double divNumTwo))
-                    {
-                        Console.Write($"{divideNumber1} / {divideNumber2} = ");
-                        Console.Write(calculator.Divide(divNumOne, divNumTwo));
-                    }
-                    else
-                    {
-                        Console.WriteLine("One or more of the numbers is not an int");
-                    }
-                    break;
+                    Console.Write($"{divideNumber1} / {divideNumber2} = ");  //original line
+                    Console.Write(calculator.Divide(divNumOne, divNumTwo)); //original line
                     break;
 
                 default:
                     Console.WriteLine("Unknown input");
                     break;
+            }
+
+            static (int, int) ValidateIntegers()
+            {
+                var inputNumber1 = Console.ReadLine();
+                var inputNumber2 = Console.ReadLine();
+
+                if (int.TryParse(inputNumber1, out int intNumber1) && int.TryParse(inputNumber2, out int intNumber2))
+                {
+                    return (intNumber1, intNumber2);
+                }
+                else
+                {
+                    throw new Exception("One or more of the numbers is not an int");  //changing Console.WriteLine to throw an exception prevents "not all code paths return value" error
+                }
             }
         }
     }
